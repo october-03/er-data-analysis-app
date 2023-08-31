6ea9ad35205f6c5b4925803ae00b164a255cdd95
october03 <season_autumn@kakao.com>
2023-08-31 09:53:18 +0900

    Initial Commit

---
+++ er-data-analysis/er_data_analysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  er_data_analysisApp.swift
+//  er-data-analysis
+//
+//  Created by 김재영 on 2023/08/31.
+//
+
+import SwiftUI
+
+@main
+struct er_data_analysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
