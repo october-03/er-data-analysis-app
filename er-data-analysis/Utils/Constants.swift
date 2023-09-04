ac799606a9681e1424178e198ca61f1b8a05e614
october03 <season_autumn@kakao.com>
2023-09-04 12:40:04 +0900

    API BaseURL 설정

---
+++ er-data-analysis/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  er-data-analysis
+//
+//  Created by 김재영 on 2023/09/04.
+//
+
+import Foundation
+
+struct Constants {
+  static let BASE_URL = Bundle.main.baseUrl
+}
---
+++ er-data-analysis/Utils/ERDATAANALYSIS++Bundle.swift
@@ -0,0 +1,18 @@
+//
+//  ERDATAANALYSIS++Bundle.swift
+//  er-data-analysis
+//
+//  Created by 김재영 on 2023/09/04.
+//
+
+import Foundation
+
+extension Bundle {
+  var baseUrl: String {
+    guard let file = self.path(forResource: "Secrets", ofType: "plist") else { return "" }
+    
+    guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+    guard let key = resouce["BASE_URL"] as ? String else {fatalError("Secrets.plist에 BaseURL 설정을 해주세요.")}
+    return key
+  }
+}
